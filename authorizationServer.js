98cf0b1cf2a6f73e1349ae2dc66c3057584f7c40
Rekicho <up201604145@fe.up.pt>
2020-05-17 01:56:04 +0100

    Generate, store & send authorization token

--- authorizationServer.js
+++ authorizationServer.js
@@ -1,49 +1,102 @@
 var express = require("express");
-var bodyParser = require('body-parser');
-var cons = require('consolidate');
-var __ = require('underscore');
-__.string = require('underscore.string');
+var bodyParser = require("body-parser");
+var cons = require("consolidate");
+var __ = require("underscore");
+__.string = require("underscore.string");
+
+const crypto = require("crypto");
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/authorizationServer');
-app.set('json spaces', 4);
+app.engine("html", cons.underscore);
+app.set("view engine", "html");
+app.set("views", "files/authorizationServer");
+app.set("json spaces", 4);
 
 // authorization server information
 var authServer = {
-	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token'
+  authorizationEndpoint: "http://localhost:9001/authorize",
+  tokenEndpoint: "http://localhost:9001/token",
 };
 
 // client information
 var clients = [
-	{
-		"client_id": "oauth-client-1",
-		"client_secret": "oauth-client-secret-1",
-		"redirect_uris": ["http://localhost:9000/callback"],
-		"scope": "foo bar"
-	}
+  {
+    client_id: "oauth-client-1",
+    client_secret: "oauth-client-secret-1",
+    redirect_uris: "http://localhost:9000/callback",
+    scope: "foo bar",
+  },
 ];
 
-var codes = {};
+var codes = [
+  {
+    code: "AAAAAAAAAAAAAAAAAAAA",
+    client_id: "oauth-client-1",
+    expires: 999999999999999999999,
+    tokens: [],
+  },
+];
 
-var requests = {};
+var requests = [];
 
-app.get('/', function(req, res) {
-	res.render('index', {clients: clients, authServer: authServer});
+app.get("/", function (req, res) {
+  res.render("index", { clients: clients, authServer: authServer });
 });
 
-app.use('/', express.static('files/authorizationServer'));
+app.use("/", express.static("files/authorizationServer"));
 
-var server = app.listen(9001, 'localhost', function () {
+app.post("/token", function (req, res) {
+  res.setHeader("Content-Type", "application/json;charset=UTF-8");
+  res.setHeader("Cache-Control", "no-store");
+  res.setHeader("Pragma", "no-cache");
+
+  if (!req.body.grant_type || !req.body.code || !req.body.client_id)
+    return res.statusCode(400).send({ error: "invalid_request" });
+
+  const client = clients.find((elem) => elem.client_id === req.body.client_id);
+  const code = codes.find((elem) => elem.code === req.body.code);
+
+  if (
+    !client ||
+    req.header("Authorization") !== "Basic " + client.client_secret
+  )
+    return res.status(400).send({ error: "invalid_client" });
+
+  if (!code || code.expires < new Date().getTime())
+    return res.status(400).send({ error: "invalid_grant" });
+
+  if (code.client_id != req.body.client_id)
+    return res.status(400).send({ error: "unauthorized_client" });
+
+  if (req.body.grant_type !== "authorization_code")
+    return res.status(400).send({ error: "unsupported_grant_type" });
+
+  const token = crypto.randomBytes(32).toString("hex");
+  const expiresIn = 3600;
+
+  code.tokens.push({
+    access_token: token,
+    expires: new Date().getTime() + expiresIn,
+  });
+
+  res.send({
+    access_token: token,
+    token_type: "Bearer",
+    expires_in: expiresIn,
+  });
+});
+
+var server = app.listen(9001, "localhost", function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('OAuth Authorization Server is listening at http://%s:%s', host, port);
+  console.log(
+    "OAuth Authorization Server is listening at http://%s:%s",
+    host,
+    port
+  );
 });
- 
