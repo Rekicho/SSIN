76e15bfe14bafc392af4867ffef7ac4a6acaf8b2
Rekicho <up201604145@fe.up.pt>
2020-05-20 15:17:57 +0100

    User auth complete

--- authorizationServer.js
+++ authorizationServer.js
@@ -1,97 +1,122 @@
 var express = require("express");
-var bodyParser = require('body-parser');
-var cons = require('consolidate');
-var __ = require('underscore');
-__.string = require('underscore.string');
+var bodyParser = require("body-parser");
+var cons = require("consolidate");
+var __ = require("underscore");
+__.string = require("underscore.string");
+
+const bcrypt = require("bcrypt");
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/authorizationServer');
-app.set('json spaces', 4);
+app.engine("html", cons.underscore);
+app.set("view engine", "html");
+app.set("views", "files/authorizationServer");
+app.set("json spaces", 4);
 
 // authorization server information
 var authServer = {
-	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token'
+  authorizationEndpoint: "http://localhost:9001/authorize",
+  tokenEndpoint: "http://localhost:9001/token",
 };
 
 // client information
 var clients = [
-	{
-		"client_id": "oauth-client-1",
-		"client_secret": "oauth-client-secret-1",
-		"redirect_uris": ["http://localhost:9000/callback"],
-		"scope": "foo bar"
-	},
-	{
-		"client_id": "oauth-client-2",
-		"client_secret": "oauth-client-secret-2",
-		"redirect_uris": ["http://localhost:9000/callback"],
-		"scope": "foo bar"
-	}
+  {
+    client_id: "oauth-client-1",
+    client_secret: "oauth-client-secret-1",
+    redirect_uris: ["http://localhost:9000/callback"],
+    scope: "foo bar",
+  },
+];
+
+const users = [
+  {
+    username: "SSIN",
+    password: "$2b$12$HGaPQ3JQOKZXyTqq8YXuBe/03Yps2LMHpiI9AEYwpQD5BujjS1BD.",
+    scope: "foo bar",
+  },
 ];
 
 var codes = {};
 
-var requests = [];
-
-app.get('/', function (req, res) {
-	res.render('index', { clients: clients, authServer: authServer });
+app.get("/", function (req, res) {
+  res.render("index", { clients: clients, authServer: authServer });
 });
 
-app.post('/token', function (req, res) {
-	const grant_type = req.params.grant_type;
-	const code = req.query.code;
-	const client_id = req.query.client_id;
-	const client_secret = req.query.client_secret;
+app.post("/token", function (req, res) {
+  const grant_type = req.params.grant_type;
+  const code = req.query.code;
+  const client_id = req.query.client_id;
+  const client_secret = req.query.client_secret;
 
-	// Verify parameters
-	console.log(req.body.info);
-	console.log(grant_type);
+  // Verify parameters
+  console.log(req.body.info);
+  console.log(grant_type);
 
-	res.send(clients);
+  res.send(clients);
 });
 
-app.use('/authorize', function (req, res) {
-	const response_type = req.query.response_type;
-	const client_id = req.query.client_id;
+app.use("/authorize", function (req, res) {
+  const response_type = req.query.response_type;
+  const client_id = req.query.client_id;
+  const error = req.query.error;
 
-	if (response_type === "code" && client_id != null) {
-		requests.push({ "client_id": client_id, "response_type": response_type });
+  const client = clients.find((elem) => elem.client_id === client_id);
 
-		res.render('authentication', { client_id: client_id, response_type: response_type });
-	} else {
-		throw new Error('PARAMETERS MISSING!!!!!');
-	}
+  if (response_type !== "code" || !client)
+    return res.status(404).send({ error: "invalid_request" });
+
+  res.render("authentication", {
+    client_id: client_id,
+    response_type: response_type,
+    error: error,
+  });
 });
 
-app.post('/submit-credentials', (req, res) => {
-	const username = req.body.identifier;
-	const password = req.body.password;
-	const client_id = req.body.client_id;
-	const response_type = req.body.response_type;
+app.post("/submit-credentials", async (req, res) => {
+  const username = req.body.identifier;
+  const password = req.body.password;
+  const client_id = req.body.client_id;
+  const response_type = req.body.response_type;
 
-	console.log(username, password, client_id, response_type);
+  const client = clients.find((elem) => elem.client_id === client_id);
+  const user = users.find((elem) => elem.username === username);
 
-	for (let i = 0; i < clients.length; i++) {
-		if (clients[i].client_id == client_id) {
-			res.redirect(clients[i].redirect_uris[0] + '?code=IDontKnowWhatToPutInHere');
-			return;
-		}
-	}
+  if (!client || !user)
+    return res.redirect(
+      "/authorize?response_type=" +
+        response_type +
+        "&client_id=" +
+        client_id +
+        "&error=BadRequest"
+    );
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match)
+    return res.redirect(
+      "/authorize?response_type=" +
+        response_type +
+        "&client_id=" +
+        client_id +
+        "&error=BadRequest"
+    );
+
+  res.redirect("/"); //Send code
 });
 
-app.use('/', express.static('files/authorizationServer'));
+app.use("/", express.static("files/authorizationServer"));
 
-var server = app.listen(9001, 'localhost', function () {
-	var host = server.address().address;
-	var port = server.address().port;
+var server = app.listen(9001, "localhost", function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-	console.log('OAuth Authorization Server is listening at http://%s:%s', host, port);
+  console.log(
+    "OAuth Authorization Server is listening at http://%s:%s",
+    host,
+    port
+  );
 });
-
--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 var express = require("express");
-var cons = require('consolidate');
-var http = require('http');
+var cons = require("consolidate");
+var http = require("http");
 
 var app = express();
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/client');
+app.engine("html", cons.underscore);
+app.set("view engine", "html");
+app.set("views", "files/client");
 
 var access_token = null;
 var refresh_token = null;
@@ -16,16 +16,19 @@ var scope = null;
 const client_id = "oauth-client-1";
 const secret_id = "oauth-client-secret-1";
 
-app.get('/', function (req, res) {
-  res.render('index', { access_token: access_token, refresh_token: refresh_token, scope: scope });
+app.get("/", function (req, res) {
+  res.render("index", {
+    client_id: client_id,
+    access_token: access_token,
+    refresh_token: refresh_token,
+    scope: scope,
+  });
 });
 
-app.get('/callback', function (req, res) {
-
+app.get("/callback", function (req, res) {
   const code = req.query.code;
 
-  if (typeof code === 'undefined')
-    throw new Error('Code not provided!');
+  if (typeof code === "undefined") throw new Error("Code not provided!");
 
   var data = JSON.stringify({
     info: {
@@ -33,43 +36,44 @@ app.get('/callback', function (req, res) {
       code: code,
       client_id: client_id,
       secret_id: secret_id,
-    }
+    },
   });
 
   var options = {
     host: server.address().address,
     port: 9001,
     auth: "Basic " + new Buffer.from("username" + ":" + "password", "base64"),
-    path: '/token',
-    method: 'POST',
+    path: "/token",
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)
-
-    }
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Content-Length": Buffer.byteLength(data),
+    },
   };
 
   var httpRequest = http.request(options, function (response) {
-    response.setEncoding('utf8');
-    response.on('data', function (chunk) {
+    response.setEncoding("utf8");
+    response.on("data", function (chunk) {
       console.log("body: " + chunk);
     });
-    response.on('end', function () {
+    response.on("end", function () {
       // res.send('ok');
-      res.render('index', { access_token: 'asdasd', refresh_token: refresh_token, scope: scope });
+      res.render("index", {
+        access_token: "asdasd",
+        refresh_token: refresh_token,
+        scope: scope,
+      });
     });
-
   });
 
   httpRequest.write(data);
   httpRequest.end();
 });
 
-app.use('/', express.static('files/client'));
+app.use("/", express.static("files/client"));
 
-var server = app.listen(9000, 'localhost', function () {
+var server = app.listen(9000, "localhost", function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
+  console.log("OAuth Client is listening at http://%s:%s", host, port);
 });
-
